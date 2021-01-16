373e9f87ee805a2b74bffeb20f5ae5c8e5f751f8
jerry411002 <71679892+jerry411002@users.noreply.github.com>
2021-01-16 11:45:58 +0800

    1

---
+++ 3D_End_107051067/Assets/Scripts/NPC.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NPC : MonoBehaviour
+{
+    [Header("NPC資料")]
+    public NPCData1   Data;
+    [Header("對話框")]
+    public GameObject dialog;
+    [Header("對話內容")]
+    public Text textContent;
+    [Header("對話者名稱")]
+    public Text textName;
+    [Header("對話間隔")]
+    public float interval;
+
+    public bool playerInarea;
+
+    public enum NPCState
+    {
+        FirstDialog, Missioning, Finish
+    }
+
+    public NPCState state = NPCState.FirstDialog;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Carino")
+        {
+            playerInarea = true;
+            StartCoroutine(dialoug());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.name == "Carino")
+        {
+            playerInarea = false;
+            Stopdialog();
+        }
+    }
+
+    private void Stopdialog()
+    {
+        dialog.SetActive(false);
+        StopAllCoroutines();
+    }
+
+    private IEnumerator dialoug()
+    {
+        dialog.SetActive(true);
+        textContent.text = "";
+        textName.text = name;
+        string dialogString = Data.dialougB;
+
+        switch (state)
+        {
+            case NPCState.FirstDialog:
+                dialogString = Data.dialougA;
+                break;  
+            case NPCState.Missioning:
+                dialogString = Data.dialougB;
+                break;
+            case NPCState.Finish:
+                dialogString = Data.dialougC;
+                break;
+        }
+
+        for (int i = 0; i < dialogString.Length; i++)
+        {
+            textContent.text += dialogString[i] + "";
+            yield return new WaitForSeconds(interval);
+        }
+    }
+}
---
+++ 3D_End_107051067/Assets/Scripts/NPC1 Data.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+[CreateAssetMenu(fileName = "NPC資訊1", menuName = "IceTalker/NPC資料")]
+
+public class NPCData1 : ScriptableObject
+{
+    [Header("第一段對話"), TextArea(1, 8)]
+    public string dialougA;
+    [Header("第二段對話"), TextArea(1, 5)]
+    public string dialougB;
+    [Header("第三段對話"), TextArea(1, 5)]
+    public string dialougC;
+
+    [Header("任務項目需求數量")]
+    public int count;
+    [Header("已取得項目需求數量")]
+    public int countCurrent;
+}
