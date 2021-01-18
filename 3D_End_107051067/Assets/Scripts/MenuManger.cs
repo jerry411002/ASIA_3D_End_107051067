4d48c8b0e07defef49483c5ef1108d6f8653c41d
jerry411002 <71679892+jerry411002@users.noreply.github.com>
2021-01-18 11:36:57 +0800

    1

---
+++ 3D_End_107051067/Assets/Scripts/MenuManger.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManger : MonoBehaviour
+{
+    public void StartGame()
+    {
+        print("開始遊戲");
+        SceneManager.LoadScene("3DGame");
+    }
+
+    public void EndGame()
+    {
+        print("離開遊戲");
+        Application.Quit();
+    }
+}
